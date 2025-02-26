40ef088898af61e80fc59ff060fb70989cd3dc29
Angie Valentina Martínez Poveda <angiemartinez224@unisangil.edu.co>
2025-02-26 15:03:55 -0500

    variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+entero = 100
+print(type(entero))
+decimal = 10.3
+print(type(decimal))
+letra = 'a'
+print(type(letra))
+mensaje = "Hola"
+print(type(mensaje))
+estado =True
+print(type(estado))
+estado =False
+print(type(estado))
