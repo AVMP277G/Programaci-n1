3562448ab517f725f01bccc272c4b24ef9ee46db
AVMP277G <angelmarpo007@gmail.com>
2025-04-02 21:13:00 -0500

    Update Estructuras_Control.py
    .

--- Estructuras_Control.py
+++ Estructuras_Control.py
@@ -1,23 +1,23 @@
 #Variable
 número = 10
 #Estructura de control If
-if numero > 5:
+if número > 5:
     print("El número es mayor a 5")
 print("Fuera de la estructura de control")
 
 #Estructura de control If-Else
-if numero > 5:
+if número > 5:
     print("El número es mayor a 5")
 else:
     print("El número es menor o igual a 5")
 
 #Estructura de control If-Elif-Else
-nuemro_1  = int(input("Ingrese un número: "))
-if numero_1 == 1:
+número_1  = int(input("Ingrese un número: "))
+if número_1 == 1:
     print("El número es 1")
-elif numero_1 == 2:
+elif número_1 == 2:
     print("El número es 2")
-elif numero_1 == 3:
+elif número_1 == 3:
     print("El número es 3")
 else:
     print("¡Error!")
