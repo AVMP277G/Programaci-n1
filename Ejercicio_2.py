cbbeb9742673f6e10f3bf199db4c162f8934c355
unknown <angiemartinez224@gmail.com>
2025-04-02 20:59:06 -0500

    clase y correcciones

---
+++ Ejercicio_2.py
@@ -0,0 +1,61 @@
+#Librerias
+import math
+ejemplo_2 = 2 * ( (4 * ( 7 + ( 4 * ( (5 * 3) - 9) ) ) ) - ( 3 * (40 - 8) ) )
+print(ejemplo_2)
+
+
+#parcial 1
+#angie
+import math
+bool= True
+bool= False
+print('Hola Bienvenido')
+print('¿Es nuevo o ya tiene cuenta?True/False:')
+if == true
+    PIN_1= int(input('Registre su PIN:'))
+    print('PIN correcto?True/False:')
+if == true 
+print('Guardar?true/false:')
+    if == true
+    print('Sesión Activada')
+    else == false
+    print('vuelve a intentarlo')
+else == false 
+print('Eliminar')
+else == false
+PIN_2= int(input('Ingrese su PIN:'))
+print('PIN correcto?true/false:')
+if == true 
+print('Es correcto?true/false:')
+    if == true
+    print('sesión iniciada')
+    else == false
+    print('vuelve a intentarlo')
+else == false 
+print('usuario bloqueado')
+#While True
+while True:
+    #Menu
+    print("lista de opciones")
+    print("1.Consultar saldo")
+    print("2. Realizar un depósito")
+    print("3. Realizar un retiro")
+    print("4. Salir")
+    #Opcion
+    1=a
+    2=b
+    3=c
+    4=d
+    opcion = input("Ingrese la opción deseada: ")
+    if opcion == a:
+        print("Consulta de saldo")
+        print("Su saldo es de:"n"$")
+    elif opcion == b:
+        print("Realizar un depósito")
+    elif opcion == c:
+        print("Realizar un retiro")
+    elif opcion == d:
+        print("Salir")
+        break
+    else:
+        print("Opción incorrecta, vuelvalo a intentarlo")
--- inicio_de_sesión.py
+++ inicio_de_sesión.py
@@ -1,5 +1,14 @@
-Angie = "AVMP"
-Contraseña = "27007"
-
-print()
-
+#Datos inicio de sesion
+Angie = "AVMP277G"
+password = 7027
+print(type(password))
+#Solicitud datos
+usuario = input("Usuario: ")
+contraseña = int(input("Contraseña: "))
+print(type(contraseña))
+#Validacion
+#Operadores de comparación
+if (Angie == usuario and password == contraseña):
+    print("Bienvenido Sistema")
+else:
+    print("Usuario o contraseña incorrectos")
