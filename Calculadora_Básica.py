0f8e83faf4abb673098c61af7beb1f57b4c5a6c2
AVMP277G <angelmarpo007@gmail.com>
2025-04-09 15:53:00 -0500

    Update Calculadora_Básica.py
    ...

--- Calculadora_Básica.py
+++ Calculadora_Básica.py
@@ -1,63 +1,51 @@
-# Calculadora con While
-# Libreria Math
+#Calculadora con while y elif
+#Libreria
 import math
-# While true
+#While True
 while True:
-    #Menú
-    print("/n Bienvenido a la Calculadora de Operaciones Básica")
-    print("Menú de opciones")
+    #Menu
+    print("Calculadora de operaciones básicas")
     print("1. Sumar")
     print("2. Restar")
     print("3. Multiplicación")
-    print("4. Divición") 
+    print("4. División")
     print("5. Potencia")
-    print("6. Raíz cuadrada")
+    print("6. Raiz cuadrada")
     print("7. Salir")
-    # Opción
-    opción = int (input("Ingrese una opción: "))
-
-if opción == 1:
-        num_1 = float(input("Ingrese el primer número: "))
-        num_2 = float(input("Ingrese el segundo número: "))
-        suma = num_1 + num_2
-        print(f"El resultado de la suma es: {suma}")
-        
-elif opción == 2:
-        num_1 = float(input("Ingrese el primer número: "))
-        num_2 = float(input("Ingrese el segundo número: "))
-        resta = num_1 - num_2
-        print(f"El resultado de la resta es: {resta}")
-
-elif opción == 3:
-        num_1 = float(input("Ingrese el primer número: "))
-        num_2 = float(input("Ingrese el segundo número: "))
-        producto= num_1 * num_2
-        print(f"El resultado de la multiplicación es: {producto}")
-
-elif opción == 4:
-        num_1 = float(input("Ingrese el primer número: "))              
-        num_2 = float(input("Ingrese el segundo número: "))
-        cociente = num_1 / num_2
-        print(f"El resultado de la división es: {cociente}")
-
-elif opción == 5:
-        num_1 = float(input("Ingrese la base: "))
-        num_2 = float(input("Ingrese la potencia: "))
-        potencia = num_1 ** num_2
-        print(f"El resultado de la potencia es: {potencia}")
-
-elif opción == 6:
-        num_1 = float(input("Ingrese la base: "))
-        raiz = math.sqrt(num_1)
-        print(f"El resultado de la raíz cuadrada es: {raiz}")  
-
-elif opción == 7:
-    print("Saliendo de la Calculadora ......")
-    
-else:
-        print("Opción errónea, intente de nuevo")
-
-
-print("Dentro del bucle de repetición")
-print("Gracias por usar la Calculadora de Operaciones Básica!!!")
-break 
+    #Opcion
+    opcion = int(input("Ingrese una opción: "))
+    if opcion == 7:
+        print("Saliendo de la Calculadora...")
+        break
+    elif opcion == 1:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        suma = num1 + num2
+        print(f"Suma = {suma}")
+    elif opcion == 2:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        resta = num1 - num2
+        print(f"Resta = {resta}")
+    elif opcion == 3:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        mul = num1 * num2
+        print(f"Multiplicación = {mul}")
+    elif opcion == 4:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        div = num1 / num2
+        print(f"División = {div}")
+    elif opcion == 5:
+        num1 = float(input("Ingrese la base: "))
+        num2 = float(input("Ingrese la potencia: "))
+        po = num1 ** num2
+        print(f"Potencia = {po}")
+    elif opcion == 6:
+        num1 = float(input("Ingrese la base para la raiz: "))
+        raiz = math.sqrt(num1)
+        print(f"Raiz cuadrada = {raiz}") 
+    else:
+        print("Opción Erronea!!!")
+print("Gracias por utilizar la Calculadora!!!")
