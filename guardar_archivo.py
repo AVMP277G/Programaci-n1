c02c09a79957b5e2dee6470c9976ff26402c34d7
AVMP277G <angelmarpo007@gmail.com>
2025-05-04 16:40:09 -0500

    Create guardar_archivo.py
    ..

---
+++ guardar_archivo.py
@@ -0,0 +1,17 @@
+#Guardar archivo en formato JSON
+#Libreria JSON
+import json
+#Guardar archivo en formato JSON
+datos = {
+        "nombre" : "Angie",
+        "apellido" : "Martínez",
+        "telefono" : 3053406861,
+        "correo" : "angiemartinez224@unisangil.edu.co",
+        "usuario": "AVMP277G"
+}
+#Variable para guardar el archivo
+archivo = open("informacion.json", "w")
+#Guardar el archivo en formato JSON
+json.dump(datos, archivo)
+#Cerrar el archivo
+archivo.close()
