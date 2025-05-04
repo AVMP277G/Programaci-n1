507a479e7ff7c2e504b6ed96cd1de68f6c64a2f5
AVMP277G <angelmarpo007@gmail.com>
2025-05-04 15:14:43 -0500

    Update Estructura_Repetición.py
    .

--- Estructura_Repetición.py
+++ Estructura_Repetición.py
@@ -1,3 +1,36 @@
+#Estructura for
+for i in range(10):#muestra un estado anterior 
+    print(i)
 
-for i in range(10):
+#Estructura for con range
+for i in range(5, 10):#muestra un estado anterior 
+    print(i)
 
+#Estructura for con range
+for i in range(3, 10, 2):#inicio, fin-1, incremento
+    print(i)
+
+#Variable
+x = "Python"
+#Separar los caracteres con for
+for letra in x:
+    if letra == "h":
+        break
+    else:
+        print(letra)
+
+#Ejemplo 1: tabla de multiplicar
+for mul in range(1,11):
+    print(f"2 x {mul} = {2 * mul}")
+
+#Ejemplo 2: tabla de multiplicar
+numero = int(input("Ingrese un número de tabla de multiplicar: "))
+for mul in range(1,11):
+    print(f"{numero} x {mul} = {numero * mul}")
+
+#Ejemplo 3: numeros pares e impares
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(f"{i} es par")
+    else:
+        print(f"{i} es impar")
